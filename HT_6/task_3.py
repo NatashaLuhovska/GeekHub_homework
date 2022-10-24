69ac70a4eaabaf88a4916fa57d7cef9c92fcf680
NatashaLuhovska <natashalugovska@gmail.com>
2022-10-24 10:18:54 +0300

    changed ht_6(3,4)

--- HT_6/task_3.py
+++ HT_6/task_3.py
@@ -42,22 +42,21 @@ def valid_username_pass(username: str, password: str):
       return True
 
 for user in users_list:
-	try:
-		valid_username_pass(user[0], user[1])
-	except LenNameException:
-		status = "The name must be at least 3 characters and no more than 50."
-	except LenPassException:
-		status = "The password must be at least 8 characters long."
-	except DigPassException:
-		status = "The password must have at least one digit"
-	except UpLowPassException:
-		status = "Password must have at least one character in upper and lower case!"
-	else:
-		status = "OK"
-	finally:
-		print(f'Name: {user[0]}')
-		print(f'Password: {user[1]}')
-		print(f'Status: {status}')
-		print('-------------------')
+   status = "OK"
+   try:
+      valid_username_pass(user[0], user[1])
+   except LenNameException:
+      status = "The name must be at least 3 characters and no more than 50."
+   except LenPassException:
+      status = "The password must be at least 8 characters long."
+   except DigPassException:
+      status = "The password must have at least one digit"
+   except UpLowPassException:
+      status = "Password must have at least one character in upper and lower case!"
+   finally:
+      print(f'Name: {user[0]}')
+      print(f'Password: {user[1]}')
+      print(f'Status: {status}')
+      print('-------------------')
 
 
