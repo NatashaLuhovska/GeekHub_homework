2912a701b5a5b477dd6daa30d1870a62dd904cb4
NatashaLuhovska <natashalugovska@gmail.com>
2023-01-25 13:31:28 +0200

    changed HT_20(added total price)

--- MyShop/cart/views.py
+++ MyShop/cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.views.decorators.http import require_http_methods
+
 from product.forms import AddProductToCartForm, ProductIdForm
 from product.models import Product
 
@@ -9,13 +10,16 @@ def view_cart(request):
     request.session.setdefault('cart', {})
     cart = request.session.get('cart')
     products = list(Product.objects.filter(id__in=cart.keys()))
+    total_price_in_cart = 0
     for product in products:
         product.in_cart_quantity = cart[str(product.id)]
         product.form_q = AddProductToCartForm(initial={'product_id': product.id, 'quantity': 1})
         product.form_id = ProductIdForm(initial={'product_id': product.id})
+        total_price_in_cart += product.current_price * product.in_cart_quantity
 
     context = {
         'products': products,
+        'total_price': total_price_in_cart,
     }
     return render(request, 'cart/index.html', context=context)
 
@@ -39,27 +43,22 @@ def add_to_cart(request):
 @require_http_methods(['POST'])
 def change_quantity(request):
     form = AddProductToCartForm(request.POST)
-    if not form.is_valid():
-        print('not valid')
-        return redirect(reverse('cart:view'))
-
-    data = form.cleaned_data
-    cart = request.session.get('cart')
-    cart[str(data['product_id'])] = data['quantity']
-    request.session.modified = True
+    if form.is_valid():
+        data = form.cleaned_data
+        cart = request.session.get('cart')
+        cart[str(data['product_id'])] = data['quantity']
+        request.session.modified = True
     return redirect(reverse('cart:view'))
 
 
 @require_http_methods(['POST'])
 def remove_product_of_cart(request):
     form = ProductIdForm(request.POST)
-    if not form.is_valid():
-        return redirect(reverse('cart:view'))
-
-    data = form.cleaned_data
-    cart = request.session.get('cart')
-    cart.pop(str(data['product_id']))
-    request.session.modified = True
+    if form.is_valid():
+        data = form.cleaned_data
+        cart = request.session.get('cart')
+        cart.pop(str(data['product_id']))
+        request.session.modified = True
     return redirect(reverse('cart:view'))
 
 
