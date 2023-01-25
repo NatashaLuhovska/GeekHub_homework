4170a2e53646421909c768c664e4656ce5088eba
NatashaLuhovska <natashalugovska@gmail.com>
2023-01-25 10:39:16 +0200

    changed HT_20 fix mistakes(cart,import,decorator)

--- MyShop/cart/views.py
+++ MyShop/cart/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, reverse
-from product.forms import AddProductToCartForm, ProductIdForm
 from django.views.decorators.http import require_http_methods
+from product.forms import AddProductToCartForm, ProductIdForm
 from product.models import Product
 
 
 def view_cart(request):
     print(request.session.items())
+    request.session.setdefault('cart', {})
     cart = request.session.get('cart')
     products = list(Product.objects.filter(id__in=cart.keys()))
     for product in products:
@@ -21,52 +22,45 @@ def view_cart(request):
 
 @require_http_methods(['POST'])
 def add_to_cart(request):
-    if request.method == 'POST':
-        form = AddProductToCartForm(request.POST)
-        if not form.is_valid():
-            return redirect(reverse('products:list'))
+    form = AddProductToCartForm(request.POST)
+    if not form.is_valid():
+        return redirect(reverse('products:list'))
 
-        data = form.cleaned_data
-        cart = request.session.setdefault('cart', {})
+    data = form.cleaned_data
+    cart = request.session.setdefault('cart', {})
+    product = Product.objects.get(id=data['product_id'])
+    if product.current_price > 0:
         cart.setdefault(str(data['product_id']), 0)
         cart[str(data['product_id'])] += data['quantity']
         request.session.modified = True
-        return redirect(reverse('products:details', kwargs={'pid': data['product_id']}))
-    else:
-        return redirect(reverse('products:list'))
+    return redirect(reverse('products:details', kwargs={'pid': data['product_id']}))
 
 
 @require_http_methods(['POST'])
 def change_quantity(request):
-    if request.method == 'POST':
-        form = AddProductToCartForm(request.POST)
-        if not form.is_valid():
-            print('not valid')
-            return redirect(reverse('cart:view'))
-
-        data = form.cleaned_data
-        cart = request.session.get('cart')
-        cart[str(data['product_id'])] = data['quantity']
-        request.session.modified = True
+    form = AddProductToCartForm(request.POST)
+    if not form.is_valid():
+        print('not valid')
         return redirect(reverse('cart:view'))
-    else:
-        return render(request, 'cart/index.html')
+
+    data = form.cleaned_data
+    cart = request.session.get('cart')
+    cart[str(data['product_id'])] = data['quantity']
+    request.session.modified = True
+    return redirect(reverse('cart:view'))
 
 
 @require_http_methods(['POST'])
 def remove_product_of_cart(request):
-    if request.method == 'POST':
-        form = ProductIdForm(request.POST)
-        if not form.is_valid():
-            return redirect(reverse('cart:view'))
-
-        data = form.cleaned_data
-        cart = request.session.get('cart')
-        cart.pop(str(data['product_id']))
-        request.session.modified = True
+    form = ProductIdForm(request.POST)
+    if not form.is_valid():
         return redirect(reverse('cart:view'))
-    else:
-        return render(request, 'cart/index.html')
+
+    data = form.cleaned_data
+    cart = request.session.get('cart')
+    cart.pop(str(data['product_id']))
+    request.session.modified = True
+    return redirect(reverse('cart:view'))
 
 
 @require_http_methods(['POST'])
